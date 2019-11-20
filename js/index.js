3a51835d80c4144eca2ecb3a0b1a081b8d576ce1
David <davidvallon@hotmail.fr>
2019-11-20 11:18:24 +0100

    random

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
 const btn = document.querySelector('button');
-const prenom = ['Tony', 'Yannick', 'David', 'Laura', 'Tristan', 'Max1','Hasna', 'Quentin', 'Rihab', 'Dylan', 'Pierre', 'Max2'];
 
-
-function random() {
-  let random = Math.round(Math.random() * (prenom.length - 1));
-
-  console.log(prenom[random]);
-
+function randomize(tab) {
+    var i, j, tmp;
+    for (i = tab.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        tmp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = tmp;
+    }
+    console.log(tab);
+    return tab;
 }
 
-btn.addEventListener('click', random);
+var tab = ['Tony', 'Yannick', 'David', 'Laura', 'Tristan', 'Max1','Hasna', 'Quentin', 'Rihab', 'Dylan', 'Pierre', 'Max2'];
+tab = randomize(tab);
+console.log(tab);
+
+btn.addEventListener('click' , () => {
+  const random = randomize(tab)
+  /**random.forEach((value, key) => {
+    document.getElementById(place-${key + 1}).innerHTML = value;
+  })*/
+
+  random.forEach(function (value, key) {
+    console.log(key, value)
+    document.getElementById('place-' + (key + 1)).innerHTML = value;
+  })
+
+
+});
